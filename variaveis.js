215569f7b73d6ba92c90f8c22d0440e5abe7b333
Gabriel-Portugal <gabrielrportugal@gmail.com>
2023-09-27 18:34:38 -0300

    Tipos de variaveis var,let e const

---
+++ variaveis.js
@@ -0,0 +1,26 @@
+//Tipo de variaveis 
+// Var pode ser usada antes de ser declarada 
+// 
+
+var altura = 5;
+var comprimento= 7;
+
+area = altura * comprimento;
+
+console.log(area);
+
+var area;
+
+//////////////
+
+let forma ='retângulo'
+let altura2 = 5;
+let comprimento2  = 7;
+
+if (forma === 'retângulo'){
+    area2 = altura2 * comprimento2;
+} else{
+    area2 = (altura2 * comprimento2) / 2;
+}
+
+console.log(area2);
