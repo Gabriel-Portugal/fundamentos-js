a461de964dc5b7e2547173d00e33493792caee33
Gabriel-Portugal <gabrielrportugal@gmail.com>
2023-09-27 11:32:25 -0300

    Comandos GitHub

---
+++ GitHub-comandos.js
@@ -0,0 +1,11 @@
+// Comandos inicialização do GitHub 
+
+/*
+    git init - Novo repositorio vazio dentro da pasta
+    git status - Verificar arquivos criados
+    git remote add origin - colar endereço do repositorio 
+    git add . - preparar os arquivos para subir para nuvem 
+    git status - 
+    git commit -m "Mensagem de texto" - O -m é de mensagem
+    git origin master 
+*/
