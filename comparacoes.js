7120c591de80b27871cd6d1a5dac6e37ce5602eb
Gabriel-Portugal <gabrielrportugal@gmail.com>
2023-09-30 08:49:06 -0300

    Estudo sobre comparações

---
+++ comparacoes.js
@@ -0,0 +1,16 @@
+// == compração implicita
+const numero = 5;
+const texto = "5";
+
+console.log( numero == texto) // so compara valor
+console.log( numero === texto) // compara vaor e tipo de dados
+
+//typeof - Informa o tipo da variavel
+console.log(typeof numero)
+console.log(typeof texto)
+
+// As boas praticas sugerem a utilização de === e conversão de modo explicito
+
+// Conversão explicita
+Number()
+String()
