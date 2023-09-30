975f243fe58fc3bec026b775fd24d0b3d85fb31b
Gabriel-Portugal <gabrielrportugal@gmail.com>
2023-09-29 23:34:03 -0300

    Final boolean e inicio conversoes

--- type-boolean.js
+++ type-boolean.js
@@ -1,12 +1,43 @@
-// Verdadeiro ou falso 
+// // Verdadeiro ou falso 
 
-const primeiroNumero = 5;
-const segundoNumero = 5;
+// const primeiroNumero = 5;
+// const segundoNumero = 5;
 
-console.log("Os numeros são iguais?" + primeiroNumero === segundoNumero);
+// console.log("Os numeros são iguais?" + primeiroNumero === segundoNumero);
 
-const texto1 = "alura";
-const texto2 = "alura";
+// const texto1 = "alura";
+// const texto2 = "alura";
 
-console.log("Os textos são igaus?" + texto1 === texto2);
+// console.log("Os textos são igaus?" + texto1 === texto2);
+
+// //Continuação aula
+
+// //bolena
+
+// const usuarioLogado = true;
+// const contaPaga = false;
+
+// //truthy or falsy
+// // 0 = false
+// // 1 = true
+
+// console.log(0 == false)
+// console.log("" == false)
+// console.log(1 == true)
+
+// null e undefined 
+// null representa vazio ou nada
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
+
 
