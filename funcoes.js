815cc60dfc2e6305f0ffd45855e475224cd3e866
Gabriel-Portugal <gabrielrportugal@gmail.com>
2023-11-06 11:57:30 -0300

    Funções

---
+++ funcoes.js
@@ -0,0 +1,20 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+//Declarar a função
+ function imprimeTexto(texto){
+     console.log(texto)
+}
+
+//Executa a função uma ou mais vezes
+
+imprimeTexto(soma());
+// imprimeTexto("Olá");
+
+//Tres firna de escrever funções 
+
+function soma(){
+    return 2 + 2;
+}
+    // console.log(soma());
